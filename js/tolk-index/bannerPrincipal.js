4fa202afdde6c80a947d8bc7a98bacb3d27397a7
amoralesh <almh_1990@hotmail.com>
2020-08-22 14:30:26 -0500

    primer commit

---
+++ js/tolk-index/bannerPrincipal.js
@@ -0,0 +1,7 @@
+$(".imagenBannerPrincipal").animate({"top":-30+"%","opacity":0},10)
+$(".imagenBannerPrincipal").animate({"top":-15+"%","opacity":1},1600)
+
+$(".textosBanner").animate({"opacity":0},10)
+$(".textosBanner").animate({"opacity":1},3600)
+
+
