febf6d337334ac0464677b08c2ff8e53febc1422
amoralesh <almh_1990@hotmail.com>
2020-09-08 12:52:20 -0500

    se erreglo el problema con el carrusel y la versión de jquery, se colocaron imagenes oficiales tdo esto el home

--- js/tolk-index/carruselMarcas.js
+++ js/tolk-index/carruselMarcas.js
@@ -1,19 +1,36 @@
+new Glide('#marcasGl', {
+  type: 'carousel',
+startAt: 0,
+autoplay: 2000,
+hoverpause: true,
+gap: 20,
+// animationTimingFunc: ease,
+  perView: 5,
+  breakpoints: {
+     1000: {
+        perView: 4
+     },
+  800: {
+        perView: 2
+     }
+  }
+}).mount();  
 
+new Glide('#marcasEx', {
+  type: 'carousel',
+startAt: 0,
+autoplay: 2000,
+hoverpause: true,
+gap: 20,
+// animationTimingFunc: ease,
+  perView: 5,
+  breakpoints: {
+     1000: {
+        perView: 4
+     },
+  800: {
+        perView: 2
+     }
+  }
+}).mount();  
 
-$(document).on('ready', function() {  
-    $(".center").slick({
-      dots: true,
-      infinite: true,
-      centerMode: true,
-      slidesToShow: 5,
-      slidesToScroll: 3
-    });
-
-    $(".regular").slick({
-     dots: true,
-     infinite: true,
-     slidesToShow: 5,
-     slidesToScroll: 1
-    });
-
-  });
