caf91d7b010755509e5a5455b47c4ab8f33f37cd
amoralesh <almh_1990@hotmail.com>
2020-09-10 21:04:23 -0500

    se repara el collapse de +/- de lista producto (falta lo mismo en la parte de filtros)

--- js/tolk-index/listaProducto.js
+++ js/tolk-index/listaProducto.js
@@ -35,6 +35,16 @@ $(document).ready(function() {
     
   });
 
+
+  function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass(' fa-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
+
 });
 
 $(".irAFiltros").click(function(){
