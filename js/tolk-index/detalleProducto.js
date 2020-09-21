f365be39f4737740391b7f3dd38ec3c814f16546
amoralesh <almh_1990@hotmail.com>
2020-09-20 22:53:47 -0500

    se agrego el collapse de cerrar detalles

--- js/tolk-index/detalleProducto.js
+++ js/tolk-index/detalleProducto.js
@@ -13,6 +13,18 @@ $( "#btnPreguntasDetalle" ).removeClass( "btnEstilo2" ).addClass( "btnEstilo1" )
 $( "#btnResenasDetalle" ).removeClass( "btnEstilo1" ).addClass( "btnEstilo2" );
   });
 
+  $( "#cd" ).click(function() {
+    $("#cd").hide();
+    $("#vd").show();
+  });
+
+  $( "#vd" ).click(function() {
+    $("#vd").hide();
+    $("#cd").show();
+  });
+
+
+
 
 
 //   btnEstilo1
