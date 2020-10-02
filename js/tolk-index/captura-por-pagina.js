56feba583a098f13776e2d0dd420140bb2fc41fa
Your Namejavier <you@example.comjavier@tolk.mx>
2020-10-01 19:01:10 -0500

    mobil captura pedido

---
+++ js/tolk-index/captura-por-pagina.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    
+    $( ".btnRecomendados" ).click(function() {
+        $("#catalagoBusqueda").css("display", "block");
+      });
+      $( ".buscar" ).click(function() {
+        $("#no_producto").css("display", "block");
+        $("#descProducto").css("display", "block");
+        
+      });
+      
+});
