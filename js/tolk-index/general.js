1ddd360b61e58e4725150d170950e6891a14b0ae
amoralesh <almh_1990@hotmail.com>
2020-10-02 01:17:57 -0500

    Se agrega 95% del home responsivo

--- js/tolk-index/general.js
+++ js/tolk-index/general.js
@@ -7,22 +7,23 @@ AOS.init({
   
 $(document).ready(function(){
 
-adjustCSS();
-$(window).resize(function(){ adjustCSS() });
 
-
+  var altoHeader =$("header").height();
+   $(".sidenav").css("top", altoHeader);
 
 
   //  $('#modalOfertaRelampago').modal('toggle');
-  // $('#modalUneteImpuls').modal({backdrop: 'static', keyboard: false});
-  // $('body').css("overflow", "hidden");
+  $('#modalUneteImpuls').modal({backdrop: 'static', keyboard: false});
+  $('body').css("overflow", "hidden");
 
-  // $('#CerrarModalUnete').click(function(){
-  //   $('body').css("overflow", "auto");
-  //   $('#modalOfertaRelampago').modal({backdrop: 'hide', keyboard: false});
-  //   $('.modal-backdrop').removeClass("modal-backdrop"); 
-  // });
+  $('#CerrarModalUnete').click(function(){
+    $('body').css("overflow", "auto");
+    // $('#modalOfertaRelampago').modal({backdrop: 'hide', keyboard: false});
+    $('.modal-backdrop').removeClass("modal-backdrop"); 
+  });
 
+
+  
 
    
 $("#fechaN").inputmask("datetime", {
@@ -43,34 +44,10 @@ $( ".cerrarS" ).click(function() {
   location.reload();
 });
 
-
 $(".open-button").click(function(){
-
-
 });
-
 $(".open-button").click(function(){
-
-
 });
-
-
-
-
-
-})
-
-function adjustCSS(){
-
-
-// if($(window).width() <= 767){
-
-//   $(".efectosRecomendados").removeAttr("data-aos");
-
-// }else{
-
-//   $(".efectosRecomendados").attr("data-aos");
-// }
 
 
 
@@ -99,7 +76,113 @@ $ ("#modalLogin").on('show.bs.modal', function (e) {
 });
 
 
+adjustCSS();
+$(window).resize(function(){ adjustCSS() });
 
+
+});
+
+
+
+function adjustCSS(){
+
+
+if($(window).width() <= 767){ 
+
+
+  new Glide('#carrucelMV', {
+    type: 'carousel',
+  startAt: 0,
+  autoplay: 442000,
+  hoverpause: true,
+  gap: 20,
+  // animationTimingFunc: ease,
+    perView: 5,
+    breakpoints: {
+       1000: {
+          perView: 4
+       },
+    800: {
+          perView: 2
+       }
+    }
+  }).mount();  
+
+}else{
+
+  $("#collapseTiendalinea").addClass("collapse");
+  $("#collapseIniciaNegocio").addClass("collapse");
+  $("#collapseAcercaNosostros").addClass("collapse");
+  
+
+
+  $("#collapseTiendalinea").removeClass("collapse");
+  $("#collapseIniciaNegocio").removeClass("collapse");
+  $("#collapseAcercaNosostros").removeClass("collapse");
+}
 
 }
 
+
+function toggleIcon(e) {
+  $(e.target)
+      .prev('.panel-heading')
+      .find(".more-less-menu")
+      .toggleClass('fa-angle-up');
+
+      $(e.target)
+      .prev('.panel-heading-s')
+      .find(".more-less-subMenu")
+      .toggleClass('fa-arrow-up');
+
+      $(e.target)
+      .prev('.panel-heading-2')
+      .find(".more-less-footer")
+      .toggleClass('fa-minus');
+}
+
+
+       $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+       $('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+
+
+       function openForm() {
+        document.getElementById("miChat").style.display = "block";
+        $(".floatWhats").css("right", "240px")
+      }
+      
+      function closeForm() {
+        document.getElementById("miChat").style.display = "none";
+        $(".floatWhats").css("right", "20px")
+      }
+
+      function openNav() {
+        document.getElementById("menuSmall").style.width = "100%";
+
+        $("#cerrarMM").show();
+        $("#abrirMM").hide(); 
+
+
+        $('body').css("overflow", "hidden");
+   
+      }
+      
+      function closeNav() {
+        document.getElementById("menuSmall").style.width = "0";
+        $("#abrirMM").show();
+        $("#cerrarMM").hide(); 
+        $('body').css("overflow", "auto");
+      }
+
+
+
+$( window ).on( "orientationchange", function( event ) {
+
+  setTimeout(function(){ 
+    var altoHeader2 =$("header").height();
+    $(".sidenav").css("top", altoHeader2);
+  
+  }, 2000);
+
+});
